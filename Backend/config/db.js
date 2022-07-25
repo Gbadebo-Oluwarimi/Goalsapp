a18ccea8fb97e0ba07845c3a28e2697e49433db9
Rimix-incognito <75817909+Rimix-incognito@users.noreply.github.com>
2022-07-25 17:01:41 +0100

    Updated the Svg Icon

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,7 @@ const connectDb = async() => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`Mongodb Connected ${conn.connection.host}`.cyan.underline);
-    } catch (error) {
+    } catch (error){
         console.log(error);
         process.exit(1);
     }
